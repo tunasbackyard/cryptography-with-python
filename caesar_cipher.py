7b2c46888db2c42d33e505b30b5f9c582ddd3d60
neglux <akcaaytuna@gmail.com>
2022-06-07 13:48:43 +0300

    implementation of caesar cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,17 @@
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+def encrypt(plaintext,key):
+    ciphertext = "";
+    for char in plaintext:
+        index = alphabet.index(char);
+        index = (index + key) % len(alphabet);
+        ciphertext += alphabet[index];
+    return ciphertext;
+
+def decrypt(ciphertext,key):
+    plaintext = "";
+    for char in ciphertext:
+        index = alphabet.index(char);
+        index = (index - key) % len(alphabet);
+        plaintext += alphabet[index];
+    return plaintext;
