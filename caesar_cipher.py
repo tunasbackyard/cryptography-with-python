cb7baf0b48ce0a788de30d3122212de7f7bbfa99
neglux <akcaaytuna@gmail.com>
2022-06-08 09:24:52 +0300

    fix: removing semicolumns

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,17 +1,17 @@
-alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 
 def encrypt(plaintext,key):
-    ciphertext = "";
+    ciphertext = ""
     for char in plaintext:
-        index = alphabet.index(char);
-        index = (index + key) % len(alphabet);
-        ciphertext += alphabet[index];
-    return ciphertext;
+        index = alphabet.index(char)
+        index = (index + key) % len(alphabet)
+        ciphertext += alphabet[index]
+    return ciphertext
 
 def decrypt(ciphertext,key):
-    plaintext = "";
+    plaintext = ""
     for char in ciphertext:
-        index = alphabet.index(char);
-        index = (index - key) % len(alphabet);
-        plaintext += alphabet[index];
-    return plaintext;
+        index = alphabet.index(char)
+        index = (index - key) % len(alphabet)
+        plaintext += alphabet[index]
+    return plaintext
